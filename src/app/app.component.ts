d1e130f5e8d040343fced6a06d2a22972351030a
David Rojo <davidrojo1997@gmail.com>
2021-08-20 01:30:24 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'cryptoAngular';
+export class AppComponent implements OnInit {
+  API_URL =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+
+  constructor(public http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(this.API_URL)
+  }
 }
