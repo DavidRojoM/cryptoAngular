bd15644003e41cb8bab0829aee5b5cff55b26529
David Rojo <davidrojo1997@gmail.com>
2021-08-20 01:42:56 +0200

    Data fetched

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core'
 import { HttpClient } from '@angular/common/http'
+import { Coin } from '../models/coin'
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,14 @@ export class AppComponent implements OnInit {
   API_URL =
     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false'
 
+  COINS: Coin[] = []
+  // eslint-disable-next-line no-useless-constructor
   constructor(public http: HttpClient) {}
 
   ngOnInit(): void {
-    this.http.get(this.API_URL)
+    this.http.get<Coin[]>(this.API_URL).subscribe((res) => {
+      // this.COINS = res
+      console.log(res)
+    })
   }
 }
---
+++ src/models/coin.ts
@@ -0,0 +1,9 @@
+export interface Coin {
+  id: String
+  symbol: String
+  name: String
+  image: String
+  current_price: number
+  price_change_percentage_24h: number
+  total_volume: number
+}
